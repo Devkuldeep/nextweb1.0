5691f13934c0e8ec1d52e3e6db9434daa2bd680b
kuldeep patel <skycreator360@gmail.com>
2024-09-02 01:47:29 +0530

    788

--- src/app/[project]/page.js
+++ src/app/[project]/page.js
@@ -8,7 +8,7 @@ export default function Project() {
     <main className="container m-auto flex min-h-screen flex-col w-full lg:p-12 p-4">
       
       <h1>project</h1>
-                                   
+                                   ``
 
       
     </main>
